41ed1492e60a9dec1525f4465cf2f44830baf5cf
WISUNGWON <tkrhkaos12@naver.com>
2020-01-20 20:27:14 +0900

    Add Files

---
+++ AdminProject/src/test/java/fastcampus/demo/FastcampusApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
